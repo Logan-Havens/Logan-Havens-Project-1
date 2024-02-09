ce80ad53aaa433f94cb816cc9d8df8b84f197960
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-02-09 21:01:39 +0000

    first try

--- server.py
+++ server.py
@@ -35,3 +35,16 @@ def serve_any_other_file(path):
     return response
 
 app.run(host='0.0.0.0',port=3000, debug=True, extra_files=['./',])
+
+# server.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3000)
+
